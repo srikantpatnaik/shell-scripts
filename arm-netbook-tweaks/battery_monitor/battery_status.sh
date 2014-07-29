16d3efacf4f42de9a87d63fd1d1a1f1d197a9594
Srikant Patnaik <srikant@gnu-linux.org>
2014-07-29 18:26:51 +0530

    modified battery upper limit

--- arm-netbook-tweaks/battery_monitor/battery_status.sh
+++ arm-netbook-tweaks/battery_monitor/battery_status.sh
@@ -11,5 +11,5 @@ voltage_now=$(cat /sys/class/power_supply/battery/voltage_now)
 voltage_min=3600
 
 # scale=2 is required for decimal upto 2 digits, 'bc' is the calculator
-notify-send  $(echo "scale=2;($voltage_now-$voltage_min)/330*100"|bc|cut -d '.' -f1)% remaining
+notify-send  $(echo "scale=2;($voltage_now-$voltage_min)/530*100"|bc|cut -d '.' -f1)% remaining
 
