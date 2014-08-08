355e5624a352e5e17bb798e158190f2cb76326f8
Srikant Patnaik <srikant@gnu-linux.org>
2014-08-08 19:51:47 +0530

    a initial version

--- arm-netbook-tweaks/chroot-xorg/chroot-xorg.sh
+++ arm-netbook-tweaks/chroot-xorg/chroot-xorg.sh
@@ -8,41 +8,61 @@
 
 chroot_path="/data/linux"
 
+function start_mount() {
+	if [ ! -f "$chroot_path/proc/uptime" ]; then
+        	mount --bind /proc $chroot_path/proc
+	fi
+
+	if [ ! -f "$chroot_path/dev/random" ]; then
+        	mount --bind /dev $chroot_path/dev
+	fi
+
+	if [ ! -d "$chroot_path/sys/kernel" ]; then
+        	mount --bind /sys $chroot_path/sys
+	fi
+
+	if [ ! -d "$chroot_path/dev/pts" ]; then
+        	mount --bind /dev/pts $chroot_path/dev/pts
+	fi
+	}
+
+function stop_mount() {
+	umount $chroot_path/proc
+	umount $chroot_path/dev
+	umount $chroot_path/dev/pts
+	umount $chroot_path/sys
+	}
+
+
 case "$1" in
 start)
-if [ ! -f "$chroot_path/proc" ]; then
-        mount --bind /proc $chroot_path/proc
-fi
-if [ ! -d "$chroot_path/dev/mapper" ]; then
-        mount --bind /dev $chroot_path/dev
-fi
-chroot /opt/chroot /bin/bash -l
+	start_mount
 
-;;
+
+	chroot $chroot_path /bin/bash -l
+	;;
+
 startx)
-if [ ! -f "/opt/chroot/proc/uptime" ]; then
-        mount --bind /proc /opt/chroot/proc
-fi
-if [ ! -d "/opt/chroot/dev/mapper" ]; then
-        mount --bind /dev /opt/chroot/dev
-fi
-#chroot /opt/chroot /bin/bash -c "runuser -l ipynb -c '/opt/rh/python27/root/usr/bin/ipython notebook --profile=nbserver &'"
-#chroot  /opt/chroot /bin/bash -c "runuser -l ipynb -c '/opt/rh/python27/root/usr/bin/ipython notebook --profile=nbserver --logfile=/home/ipynb/.ipython/profile_nbserver/log/commands.log --debug &> /home/ipynb/.ipython/profile_nbserver/log/ipynb-all.log &'"
+	start_mount
+
+	if [ ! -f $chroot_path/dev/fb0 ]; then
+ 		ln -s /dev/graphics/fb0 /dev/fb0
+	fi
+ 	# kill android UI
+	#setprop ctl.stop media & setprop ctl.stop zygote
+
+	#chroot /opt/chroot /bin/bash -c "runuser -l ipynb -c '/opt/rh/python27/root/usr/bin/ipython notebook --profile=nbserver &'"
 ;;
 
 stop)
-umount $chroot_path/proc
-umount $chroot_path/dev
-umount $chroot_path/dev/pts
-umount $chroot_path/sys
+	stop_mount
 ;;
 
 stopx)
-#chroot /opt/chroot /bin/bash -c "runuser -l ipynb -c 'killall ipython'"
-#sleep 3
-#echo "stopped ipython server and unmounting proc and dev"
-#umount /opt/chroot/proc
-#umount /opt/chroot/dev
+	#chroot /opt/chroot /bin/bash -c "runuser -l ipynb -c 'killall ipython'"
+	#sleep 3
+	#echo "stopped ipython server and unmounting proc and dev"
+	stop_mount
 ;;
 *)
 
