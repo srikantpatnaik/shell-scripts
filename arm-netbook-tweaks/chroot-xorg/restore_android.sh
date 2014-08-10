742652af796582469110639d2d56ef0b0c9bd62b
Srikant Patnaik <srikant@gnu-linux.org>
2014-08-11 00:39:04 +0530

    commenting for future me

--- arm-netbook-tweaks/chroot-xorg/restore_android.sh
+++ arm-netbook-tweaks/chroot-xorg/restore_android.sh
@@ -1,7 +1,5 @@
 #!/system/bin/sh
-#
-# for WM8880 linux xorg chroot
-
+# A simple unmount script
 chroot_path=/data/linux
 
 stop_mount() {
@@ -13,6 +11,7 @@ stop_mount() {
 
 
         stop_mount
+	# Start Android again (assuming Xorg has been stopped)
         setprop ctl.start media & setprop ctl.start zygote & setprop ctl.start surfaceflinger & setprop ctl.start drm
 
 
